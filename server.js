f71aea62e6e859fd56ee723ac7f7582b5ef70826
Prashanth R <rprashanth1211@gmail.com>
2023-04-28 23:53:04 +1000

    SIT 737 - Updated 737

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ const mul= (n1,n2) => {
 const div= (n1,n2) => {
   return n1/n2;
 }
+const power= (n1,n2) => {
+  return n1*n2;
+}
 app.get("/add", (req,res)=>{
     try{
     const n1= parseFloat(req.query.n1);
@@ -140,6 +143,37 @@ app.get("/division", (req,res)=>{
   }
   logger.info('Parameters '+n1+' and '+n2+' received for division');
   const result = div(n1,n2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+app.get("/power", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  if(n2 == 0)
+  {
+    logger.error("n2 is Zero");
+      throw new Error("Zero division error");
+  }
+  
+  if (n1 === NaN || n2 === NaN) {
+      console.log()
+      throw new Error("Parsing Error");
+  }
+  logger.info('Parameters '+n1+' and '+n2+' received for division');
+  const result = power(n1,n2);
   res.status(200).json({statuscocde:200, data: result }); 
   } catch(error) { 
       console.error(error)
